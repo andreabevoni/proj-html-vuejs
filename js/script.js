5e21f34ca1c66537a6eab6c89e76c574f5d5431e
Andrea Bevoni <andreabevoni@gmail.com>
2020-12-09 15:04:59 +0100

    Inizio struttura header usando vue per stampare i link a video

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// utilizzo Vue per inserire i link nell'header e nel footer
+var app = new Vue({
+  el: "#root",
+  data: {
+    headerLinks: ["HOME", "PAGES", "MENU", "EVENT", "BLOG", "LANDING"],
+  }
+});

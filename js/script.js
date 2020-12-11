f324bc290fad27efc2fb801eee4337433e356777
Andrea Bevoni <andreabevoni@gmail.com>
2020-12-11 01:20:16 +0100

    Entrambi i carousel funzionanti. Manca refactoring.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@ var app = new Vue({
   data: {
     logo: "img/h5-logo-divided-header.png",
     headerLinks: ["ORDER ONLINE", "HOME", "PAGES", "MENU", "EVENT", "BLOG", "LANDING", "CART", "SEARCH"],
+    jumboIndex: 0,
+    jumboImages: ["img/h3-rev-img-2.png", "img/h3-rev-img-4.png", "img/h3-rev-img-6.png"],
+    jumboBgs: ["firstbg", "secondbg", "thirdbg"],
+    quoteIndex: 0,
+    quotes: [
+      {
+        journal: 'WASHINGTON POST 2018',
+        quote: '"FORGET THE TRENDY PIZZA SHOPS, THIS HIDDEN SPOT MAKES THE BEST NEW YORK-STYLE PIZZA SLICE IN NAPLES"'
+      },
+      {
+        journal: 'THE GUARDIAN 2020',
+        quote: '"HIGH PRICES AND HIGH QUALITY, THIS PLACE IS WHERE YOU WANT TO EAT IF YOU ARE NO PEASANT"'
+      },
+      {
+        journal: 'CORRIERE DELLA SERA 2019',
+        quote: '"PINEAPPLE PIZZA IS A SIN AGAINST HUMANITY, PLEASE NEVER EAT IN THIS PLACE IF YOU ARE ITALIAN"'
+      }
+    ],
     restaurants: {
       name: 'FIND OUR RESTAURANTS',
       list: [
@@ -66,6 +84,28 @@ var app = new Vue({
     credits: {
       firstPart: 'Created with &#10084; by',
       secondPart: 'Qode Interactive'
+    }
+  },
+  methods: {
+    // funzione immagine jumbotron precedente
+    prevImage: function() {
+      this.jumboIndex = this.jumboIndex === 0 ? this.jumboImages.length - 1 : this.jumboIndex - 1;
+    },
+    // funzione immagine jumbotron successiva
+    nextImage: function() {
+      this.jumboIndex = this.jumboIndex === this.jumboImages.length - 1 ? 0 : this.jumboIndex + 1;
+    },
+    // funzione citazione precedente
+    prevQuote: function() {
+      this.quoteIndex = this.quoteIndex === 0 ? this.quotes.length - 1 : this.quoteIndex - 1;
+    },
+    // funzione citazione successiva
+    nextQuote: function() {
+      this.quoteIndex = this.quoteIndex === this.quotes.length - 1 ? 0 : this.quoteIndex + 1;
+    },
+    // funzione salta citazione al pallino relativo
+    selectQuote: function(i) {
+      this.quoteIndex = i;
     }
   }
 });
